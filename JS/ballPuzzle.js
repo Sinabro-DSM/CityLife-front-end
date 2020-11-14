f2762fc90aa4e8221ea83010c8354867caec929e
Srim <deamoon0801@naver.com>
2020-11-15 00:57:53 +0900

    [MODIFY] token

--- JS/ballPuzzle.js
+++ JS/ballPuzzle.js
@@ -141,7 +141,7 @@ function set(){
                                 method: 'post',
                                 url: 'http://13.125.38.255:3000/game/rank/money',
                                 headers: { 
-                                  'access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VySWQiLCJpYXQiOjE2MDUyNzU2NjAsImV4cCI6MzYwMDE2MDUyNzU2NjB9.M4il0CtNPjghIydNyZy-ghN89G__8exyVSxQtjOIm6g' 
+                                  'access-token': localStorage.getItem('accessToken') 
                                 },
                                 data :giveMoney
                               })
