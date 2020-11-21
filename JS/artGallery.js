6c390416607d641693ab6826dd8496e8439e691b
seockhun <hun00719@gmail.com>
2020-11-21 18:05:39 +0900

    [ADD] 미술관 JS 새로고침

---
+++ JS/artGallery.js
@@ -0,0 +1,66 @@
+let groupPhoto = document.getElementById('group');
+let yallyPhoto = document.getElementById('yally');
+let talkPhoto = document.getElementById('talk');
+let firstBtn = document.getElementById('firstBtn');
+let secondBtn = document.getElementById('secondBtn');
+let thirdBtn = document.getElementById('thirdBtn');
+
+firstBtn.addEventListener('click', ()=>{
+    if(yallyPhoto.style.transform == "translateX(-102.7%)"){
+        groupPhoto.style.zIndex = '2';
+        groupPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transition = '0.3s';
+        groupPhoto.style.transform = "translateX(0%)";
+        yallyPhoto.style.transform = "translateX(0%)";
+        talkPhoto.style.transform = "translateX(0%)";
+    }
+    else if(yallyPhoto.style.transform == 'translateX(102.7%)'){
+        groupPhoto.style.transition = '0.3s';
+        groupPhoto.style.zIndex = '2';
+        talkPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transform = 'translateX(0%)';
+        groupPhoto.style.transform = 'translateX(0%)';
+        talkPhoto.style.transform = 'translateX(0%)';
+    }
+});
+
+secondBtn.addEventListener('click', ()=>{
+    if(yallyPhoto.style.transform == 'translateX(102.7%)'){
+        groupPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transition = '0.3s';
+        talkPhoto.style.transition = '0.3s';
+        talkPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transition = '0.3s';
+        yallyPhoto.style.zIndex = '2';
+        yallyPhoto.style.transform = 'translateX(-102.7%)';
+        talkPhoto.style.transform = 'translateX(102.7%)';
+        groupPhoto.style.transform = 'translateX(102.7%)';
+    }
+    else{
+        groupPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transition = '0.3s';
+        talkPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transform = 'translateX(-102.7%)';
+        groupPhoto.style.transform = 'translateX(205.14%)';
+        talkPhoto.style.transform = 'translateX(-102.7%)';
+    }
+});
+
+thirdBtn.addEventListener('click', ()=>{
+    if(yallyPhoto.style.transform == 'translateX(-102.7%)'){
+        groupPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transition = '0.3s';
+        talkPhoto.style.transition = '0.3s';
+        groupPhoto.style.transform = 'translateX(102.7%)';
+        yallyPhoto.style.transform = 'translateX(102.7%)';
+        talkPhoto.style.transform = 'translateX(-205.14%)';
+    }
+    else if(yallyPhoto.style.transform = 'translateX(0%)'){
+        groupPhoto.style.transition = '0.3s';
+        yallyPhoto.style.transition = '0.3s';
+        talkPhoto.style.transition = '0.3s';
+        groupPhoto.style.transform = 'translateX(102.7%)';
+        yallyPhoto.style.transform = 'translateX(102.7%)';
+        talkPhoto.style.transform = 'translateX(-205.14%)';
+    }
+});
